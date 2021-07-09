7983e3c1e858522a4f26af5564dc4f1b163cd966
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2021-07-09 19:31:11 +0000

    add (again and again 😜) MongoDB settings #6

--- indiekit.config.js
+++ indiekit.config.js
@@ -80,6 +80,9 @@ indiekit.set('publication.postTypes', [
   }
 ]);
 
+// Application settings
+indiekit.set('application.mongodbUrl', process.env.MONGODB_URL);
+
 // Configure publication
 indiekit.set('publication.categories', 'https://alienlebarge.ch/archives/categories.json');
 indiekit.set('publication.me', 'https://alienlebarge.ch');
