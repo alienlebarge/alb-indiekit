de4701672f07e26fefeeafe40e80e5b7b5d5304e
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2022-02-07 09:32:32 +0100

    remove default options for twitter syndication

--- indiekit.config.js
+++ indiekit.config.js
@@ -27,11 +27,7 @@ const mastodon = new MastodonSyndicator({
 const twitter = new TwitterSyndicator({
   checked: true,
   forced: true,
-  user: 'alienlebarge',
-  apiKey: process.env.TWITTER_API_KEY,
-  apiKeySecret: process.env.TWITTER_API_KEY_SECRET,
-  accessToken: process.env.TWITTER_ACCESS_TOKEN,
-  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  user: 'alienlebarge'
 });
 
 // Configure Jekyll publication preset
