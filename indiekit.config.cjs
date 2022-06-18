f43db9cd4ec150b8cb323137f31b1438f8ad1e02
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2022-06-18 19:58:04 +0000

    beautify

--- indiekit.config.cjs
+++ indiekit.config.cjs
@@ -14,63 +14,70 @@ module.exports = {
     categories: "https://alienlebarge.ch/archives/categories.json",
     locale: "fr-CH",
     me: "https://alienlebarge.ch",
-    postTypes:  [{
-      type: "article",
-      name: "Article",
-      post: {
-        path: "src/articles/{yyyy}-{MM}-{dd}-{slug}.md",
-        url: "articles/{yyyy}/{MM}/{slug}"
+    postTypes: [
+      {
+        type: "article",
+        name: "Article",
+        post: {
+          path: "src/articles/{yyyy}-{MM}-{dd}-{slug}.md",
+          url: "articles/{yyyy}/{MM}/{slug}",
+        },
+        media: {
+          path: "src/media/articles/{yyyy}/{MM}/{dd}/{slug}/{filename}",
+          url: "media/articles/{yyyy}/{MM}/{dd}/{slug}/{filename}",
+        },
       },
-      media: {
-        path: "src/media/articles/{yyyy}/{MM}/{dd}/{slug}/{filename}",
-        url: "media/articles/{yyyy}/{MM}/{dd}/{slug}/{filename}"
-      }
-    }, {
-      type: "note",
-      name: "Note",
-      post: {
-        path: "src/notes/{yyyy}{MM}{dd}{HH}{mm}{ss}.md",
-        url: "notes/{yyyy}{MM}{dd}{HH}{mm}{ss}"
-      }
-    }, {
-      type: "photo",
-      name: "Photo",
-      post: {
-        path: "src/photos/{yyyy}-{MM}-{dd}-{slug}.md",
-        url: "photos/{yyyy}/{MM}/{slug}"
+      {
+        type: "note",
+        name: "Note",
+        post: {
+          path: "src/notes/{yyyy}{MM}{dd}{HH}{mm}{ss}.md",
+          url: "notes/{yyyy}{MM}{dd}{HH}{mm}{ss}",
+        },
       },
-      media: {
-        path: "src/media/photos/{yyyy}/{MM}/{dd}/{filename}",
-        url: "media/photos/{yyyy}/{MM}/{dd}/{filename}"
-      }
-    }, {
-      type: "bookmark",
-      name: "Bookmark",
-      post: {
-        path: "src/bookmarks/{yyyy}-{MM}-{dd}-{slug}.md",
-        url: "bookmarks/{yyyy}/{MM}/{slug}"
-      }
-    }, {
-      type: "reply",
-      name: "Reply",
-      post: {
-        path: "src/replies/{yyyy}{MM}{dd}{HH}{mm}{ss}.md",
-        url: "replies/{yyyy}{MM}{dd}{HH}{mm}{ss}"
-      }
-    }, {
-      type: "like",
-      name: "Like",
-      post: {
-        path: 'src/likes/{yyyy}-{MM}-{dd}-{slug}.md',
-        url: 'likes/{slug}'
-      }
-    }],
+      {
+        type: "photo",
+        name: "Photo",
+        post: {
+          path: "src/photos/{yyyy}-{MM}-{dd}-{slug}.md",
+          url: "photos/{yyyy}/{MM}/{slug}",
+        },
+        media: {
+          path: "src/media/photos/{yyyy}/{MM}/{dd}/{filename}",
+          url: "media/photos/{yyyy}/{MM}/{dd}/{filename}",
+        },
+      },
+      {
+        type: "bookmark",
+        name: "Bookmark",
+        post: {
+          path: "src/bookmarks/{yyyy}-{MM}-{dd}-{slug}.md",
+          url: "bookmarks/{yyyy}/{MM}/{slug}",
+        },
+      },
+      {
+        type: "reply",
+        name: "Reply",
+        post: {
+          path: "src/replies/{yyyy}{MM}{dd}{HH}{mm}{ss}.md",
+          url: "replies/{yyyy}{MM}{dd}{HH}{mm}{ss}",
+        },
+      },
+      {
+        type: "like",
+        name: "Like",
+        post: {
+          path: "src/likes/{yyyy}-{MM}-{dd}-{slug}.md",
+          url: "likes/{slug}",
+        },
+      },
+    ],
     slugSeparator: "-",
     timeZone: "Europe/Zurich",
   },
   "@indiekit/store-github": {
     user: "alienlebarge",
-    repo: "alienlebarge-v3"
+    repo: "alienlebarge-v3",
   },
   "@indiekit/syndicator-mastodon": {
     checked: true,
