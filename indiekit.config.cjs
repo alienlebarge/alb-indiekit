93b4fa8b3240f5d5f0ea5507d71323877871b1c0
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2024-03-21 07:28:53 +0100

    update dependencies - indiekit 1.0.0-beta.9

--- indiekit.config.cjs
+++ indiekit.config.cjs
@@ -16,67 +16,74 @@ module.exports = {
     me: "https://alienlebarge.ch",
     postTypes: [
       {
-        type: "article",
-        name: "Article",
-        post: {
-          path: "src/articles/{yyyy}/{MM}/{slug}/index.md",
-          url: "articles/{yyyy}/{MM}/{slug}",
-        },
-        media: {
-          path: "src/articles/{yyyy}/{MM}/{slug}/{filename}",
-          url: "media/articles/{yyyy}/{MM}/{slug}/{filename}",
-        },
-      },
-      {
-        type: "note",
-        name: "Note",
-        post: {
-          path: "src/notes/{yyyy}{MM}{dd}{HH}{mm}{ss}.md",
-          url: "notes/{yyyy}{MM}{dd}{HH}{mm}{ss}",
+        article: {
+          post: {
+            name: "Article",
+            path: "src/articles/{yyyy}/{MM}/{slug}/index.md",
+            url: "articles/{yyyy}/{MM}/{slug}",
+          },
+          media: {
+            path: "src/articles/{yyyy}/{MM}/{slug}/{filename}",
+            url: "media/articles/{yyyy}/{MM}/{slug}/{filename}",
+          },
         },
       },
       {
-        type: "photo",
-        name: "Photo",
-        post: {
-          path: "src/photos/{yyyy}-{MM}-{dd}-{slug}.md",
-          url: "photos/{yyyy}/{MM}/{slug}",
-        },
-        media: {
-          path: "src/media/photos/{yyyy}/{MM}/{dd}/{filename}",
-          url: "media/photos/{yyyy}/{MM}/{dd}/{filename}",
+        note: {
+          post: {
+            name: "Note",
+            path: "src/notes/{yyyy}{MM}{dd}{HH}{mm}{ss}.md",
+            url: "notes/{yyyy}{MM}{dd}{HH}{mm}{ss}",
+          },
         },
       },
       {
-        type: "bookmark",
-        name: "Bookmark",
-        post: {
-          path: "src/bookmarks/{yyyy}-{MM}-{dd}-{slug}.md",
-          url: "bookmarks/{yyyy}/{MM}/{slug}",
+        photo:{
+          post: {
+            name: "Photo",
+            path: "src/photos/{yyyy}-{MM}-{dd}-{slug}.md",
+            url: "photos/{yyyy}/{MM}/{slug}",
+          },
+          media: {
+            path: "src/media/photos/{yyyy}/{MM}/{dd}/{filename}",
+            url: "media/photos/{yyyy}/{MM}/{dd}/{filename}",
+          },
         },
       },
       {
-        type: "reply",
-        name: "Reply",
-        post: {
-          path: "src/replies/{yyyy}{MM}{dd}{HH}{mm}{ss}.md",
-          url: "replies/{yyyy}{MM}{dd}{HH}{mm}{ss}",
+        bookmark: {
+          post: {
+            name: "Bookmark",
+            path: "src/bookmarks/{yyyy}-{MM}-{dd}-{slug}.md",
+            url: "bookmarks/{yyyy}/{MM}/{slug}",
+            },
         },
       },
       {
-        type: "like",
-        name: "Like",
-        post: {
-          path: "src/likes/{yyyy}-{MM}-{dd}-{slug}.md",
-          url: "likes/{slug}",
+        reply: {
+          post: {
+            name: "Reply",
+            path: "src/replies/{yyyy}{MM}{dd}{HH}{mm}{ss}.md",
+            url: "replies/{yyyy}{MM}{dd}{HH}{mm}{ss}",
+          },
         },
       },
       {
-        type: "repost",
-        name: "Repost",
-        post: {
-          path: "src/reposts/{yyyy}-{MM}-{dd}-{slug}.md",
-          url: "reposts/{slug}",
+        like: {
+          post: {
+            name: "Like",
+            path: "src/likes/{yyyy}-{MM}-{dd}-{slug}.md",
+            url: "likes/{slug}",
+          },
+        },
+      },
+      {
+        repost: {
+          post: {
+            name: "Repost",
+            path: "src/reposts/{yyyy}-{MM}-{dd}-{slug}.md",
+            url: "reposts/{slug}",
+          },
         },
       },
     ],
